52232fefe590d164a0f1316c4712f42b257f429f
ctrlnot <tonton.perocho@gmail.com>
2019-10-08 21:50:35 +0800

    Added yttomp3 and souncloud downloader scripts.

---
+++ scripts/sc.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+sclink=""
+outputFilename=""
+title=""
+artist=""
+album=""
+cover=""
+
+while getopts l:o:t:r:b:c flag; do
+  case "${flag}" in
+    l) sclink="${OPTARG}";;
+    o) outputFilename="${OPTARG}";;
+    t) title="${OPTARG}";;
+    r) artist="${OPTARG}";;
+    b) album="${OPTARG}";;
+    c) cover="${OPTARG}";;
+  esac
+done
+
+if [ -z "$sclink" ]
+  then
+    echo "No soundcloud link is passed! Exiting..."
+    exit 1
+fi
+
+if [ -z "$outputFilename" ]
+  then
+    echo "Output filename is required! Exiting..."
+    exit 1
+fi
+
+youtube-dl "$sclink" --add-metadata --print-json > metadata
+
+tempFilename=$(jq -r "._filename" metadata)
+outputExt=$(jq -r ".ext" metadata)
+outputFilename="$outputFilename.mp3"
+tempFileNameConvert="t.mp3"
+args=("-i" "$tempFilename")
+
+thumbnail=$(echo $(jq ".thumbnail" metadata.json) | sed 's/\"//g')
+original=$(echo $thumbnail | sed 's/t500x500/original/g')
+
+wget -qO thumbnailCover "$thumbnail"
+wget -qO originalCover "$original"
+
+cover=thumbnailCover
+
+if [ -f originalCover ]
+  then
+    originalWidth=$(identify -format '%w' originalCover)
+    if [ "$originalWidth" -gt 500 ]
+      then
+        rm thumbnailCover
+        cover=originalCover
+
+        if [ "$originalWidth" -gt 1400 ]
+          then
+            convert -debug None originalCover -resize 1400x1400 originalCover
+        fi
+      else
+        rm originalCover
+    fi
+fi
+args+=("-i" "$cover" "-map" "0:0" "-map" "1:0" "-c" "copy" "-id3v2_version" "3" "-metadata:s:v" "title=Album Cover" "-metadata:s:v" "comment=Cover (front)")
+
+if ! [ -z "$title" ]
+  then
+    args+=("-metadata" "title=$title")
+fi
+
+if ! [ -z "$artist" ]
+  then
+    args+=("-metadata" "artist=$artist" "-metadata" "album_artist=$artist")
+fi
+
+if ! [ -z "$album" ]
+  then
+    args+=("-metadata" "album=$album")
+fi
+
+args+=("-metadata" "comment=Source: $sclink")
+
+if [ "$outputExt" != "mp3" ]
+  then
+    ffmpeg -loglevel quiet -i "$tempFilename" -ab 320k "$tempFileNameConvert"
+    args[1]="$tempFileNameConvert"
+fi
+
+ffmpeg -loglevel quiet "${args[@]}" -acodec copy "$outputFilename"
+
+[ -f $tempFileNameConvert ] && rm $tempFileNameConvert
+rm $tempFilename
+rm metadata
---
+++ scripts/yttomp3.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+ytlink=""
+outputFileName=""
+title=""
+artist=""
+album=""
+cover=""
+start="0"
+end="0"
+
+while getopts l:o:t:r:b:c:s:e: flag; do
+  case "${flag}" in
+    l) ytlink="${OPTARG}";;
+    o) outputFileName="${OPTARG}";;
+    t) title="${OPTARG}";;
+    r) artist="${OPTARG}";;
+    b) album="${OPTARG}";;
+    c) cover="${OPTARG}";;
+    s) start="${OPTARG}";;
+    e) end="${OPTARG}";;
+  esac
+done
+
+if [ -z "$ytlink" ]
+  then
+    echo "No youtube link is passed! Exiting..."
+    exit 1
+fi
+
+if [ -z "$outputFileName" ]
+  then
+    echo "Output filename is required! Exiting..."
+    exit 1
+fi
+
+youtube-dl "$ytlink" --add-metadata --extract-audio --audio-format mp3 --output "temp.%(ext)s"
+
+tempFilename="temp.mp3"
+outputFileName="$outputFileName.mp3"
+args=("-i" "$tempFilename")
+
+if [ "$start" != "0" ] || [ "$end" != "0" ]
+  then
+    # somehow if trimming and adding cover is included on param...
+    # the output file has no cover...idk the reason :(
+    # this part separates the trimming function and now it works!
+    toTrimFileName="totrim.mp3"
+    cp "$tempFilename" "$toTrimFileName"
+    rm "$tempFilename"
+    ffmpeg -loglevel quiet -i "$toTrimFileName" -ss "$start" -to "$end" -acodec copy "$tempFilename"
+    rm "$toTrimFileName"
+fi
+
+if ! [ -z "$cover" ]
+  then
+    if [ "$cover" = "true" ]
+      then
+        echo "[ffmpeg] Embedding youtube thumbnail as cover art on track..."
+        youtube-dl "$ytlink" --write-thumbnail --skip-download --output "rawcover.jpg"
+        convert -size 1280x1280 xc:transparent transparent.png
+        composite -gravity center rawcover.jpg transparent.png coverart.png
+        rm transparent.png
+    fi
+
+    if [ "$cover" = "center" ]
+      then
+        echo "[ffmpeg] Embedding youtube thumbnail center as cover art on track..."
+        youtube-dl "$ytlink" --write-thumbnail --skip-download --output "rawcover.jpg"
+        convert "rawcover.jpg" -crop 720x720+280+0 coverart.png
+    fi
+
+    rm rawcover.jpg
+    cover="coverart.png"
+    args+=("-i" "$cover" "-map" "0:0" "-map" "1:0" "-c" "copy" "-id3v2_version" "3" "-metadata:s:v" "title=Album Cover" "-metadata:s:v" "comment=Cover (front)")
+fi
+
+args+=("-metadata" "title=$title" "-metadata" "artist=$artist" "-metadata" "album=$album" "-metadata" "comment=Source: $ytlink")
+ffmpeg -loglevel quiet "${args[@]}" -acodec copy "$outputFileName"
+
+rm $tempFilename
+rm $cover
