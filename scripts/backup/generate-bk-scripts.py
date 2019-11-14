a2e577978323dade982f8e62adb8f9eaf24f40d7
ctrlnot <jtperocho+dev@protonmail.com>
2019-11-14 21:53:46 +0800

    Fixed dates on backup script.

--- scripts/backup/generate-bk-scripts.py
+++ scripts/backup/generate-bk-scripts.py
@@ -2,9 +2,6 @@ import json
 import datetime
 
 purgatory_folder_name = '000'
-today = datetime.date.today().strftime('%Y%m%d')
-last_month = datetime.date.today() - datetime.timedelta(days=30)
-last_month = last_month.strftime('%Y%m%d')
 
 with open('scripts/backup/config.json') as config_data:
   config = json.load(config_data)
@@ -26,21 +23,23 @@ with open('scripts/backup/config.json') as config_data:
 
       script_file.write('#!/bin/bash\n')
 
-      script_file.write(f'\nrsync -avP{str_bool_opts} --backup --backup-dir="{backup_dir}/{today}" "{source}/" "{dest}"\n')
+      script_file.write('\ntoday=$(date +"%Y%m%d")\n')
+
+      script_file.write(f'\nrsync -avP{str_bool_opts} --backup --backup-dir="{backup_dir}/$today" "{source}/" "{dest}"\n')
 
       if options.get('hasPurgatory'):
-        script_file.write(f'\nrsync -avP --delete --backup --backup-dir="{backup_dir}/{today}/{purgatory_folder_name}" "{source}/{purgatory_folder_name}/" "{dest}/{purgatory_folder_name}"\n')
+        script_file.write(f'\nrsync -avP --delete --backup --backup-dir="{backup_dir}/$today/{purgatory_folder_name}" "{source}/{purgatory_folder_name}/" "{dest}/{purgatory_folder_name}"\n')
 
       if options['cleanVersions']:
         script_file.write(f'\ntarget="{backup_dir}"\n')
-        script_file.write(f'dlastmonth=$(date -d "{last_month}" +"%Y%m%d")\n')
+        script_file.write('lastMonth=$(date --date="-1 month" +"%Y%m%d")\n')
         script_file.write('fs=($(ls "$target"))\n')
 
         script_file.write('\nfor dirDate in "${fs[@]}"\n')
         script_file.write('do\n')
         script_file.write('  folder=$(basename "$dirDate")\n')
-        script_file.write('  if [ $(date -d "$folder" +"%Y%m%d") -lt "$dlastmonth" ]; then\n')
-        script_file.write(f'    echo "Deleting Folder Version: $target/$folder"\n')
+        script_file.write('  if [ $(date -d "$folder" +"%Y%m%d") -lt "$lastMonth" ]; then\n')
+        script_file.write('    echo "Deleting Folder Version: $target/$folder"\n')
         script_file.write('    rm -r "$target/$folder"\n')
         script_file.write('  fi\n')
         script_file.write('done\n')
