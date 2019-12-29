dca0ce6d592cfd90bbe30bd924649d8d81352ef0
ctrlnot <jtperocho+dev@protonmail.com>
2019-12-29 15:58:14 +0800

    Added script to convert flac to mp3 and create music tree in txt.

---
+++ scripts/flac-to-mp3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Source: https://stackoverflow.com/questions/26109837/convert-flac-to-mp3-with-ffmpeg-keeping-all-metadata
+
+ffmpeg -i $1 -ab 320k -map_metadata 0 -id3v2_version 3 $2
