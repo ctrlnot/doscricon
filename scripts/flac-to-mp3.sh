17690ac120a6d5b0bf5c05147104a145a5d2e9f7
ctrlnot <jtperocho+dev@protonmail.com>
2020-03-13 23:52:19 +0800

    Fixed parsing text on flac to mp3 script.

--- scripts/flac-to-mp3.sh
+++ scripts/flac-to-mp3.sh
@@ -2,4 +2,4 @@
 
 # Source: https://stackoverflow.com/questions/26109837/convert-flac-to-mp3-with-ffmpeg-keeping-all-metadata
 
-ffmpeg -i $1 -ab 320k -map_metadata 0 -id3v2_version 3 $2
+ffmpeg -i "$1" -ab 320k -map_metadata 0 -id3v2_version 3 "$2"
